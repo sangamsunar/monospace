54fb472a868ad1fc6a2acedd8588596776c7505f
Sangam Sunar <s.sangam.00007@gmail.com>
2026-01-23 01:06:04 +0545

    initial push

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -3,23 +3,27 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-try {
+  try {
     res.status(200).send("You got 21 notes");
-  
-}catch (error) {
+  } catch (error) {
     console.error("Server Error ", error);
   }
+});
 
 router.post("/:id", (req, res) => {
-try {
+  try {
     res.status(210).json({ message: "Note Created Successfully" });
-  
-}catch (error) {
+  } catch (error) {
     console.error("Server Error ", error);
-  }});
+  }
+});
 
 router.put("/:id", (req, res) => {
-  res.status(200).json({ message: "Note Updates Successfully" });
+  try {
+    res.status(200).json({ message: "Note Updates Successfully" });
+  } catch (error) {
+    console.error("Server Error ", error);
+  }
 });
 
 router.delete("/:id", (req, res) => {
